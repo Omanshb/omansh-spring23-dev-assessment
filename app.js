e38155498821e308a800e5ff7f14e90136f29b09
Omanshb <73659149+Omanshb@users.noreply.github.com>
2024-08-20 16:38:51 -0400

    Level 0 Tasks

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { connectDB } from './mongodb.js';
 
 dotenv.config();
 const app = express();
-const APP_PORT = 5000;
+const APP_PORT = 3000;
 app.use(cors({ origin: true }));
 
 app.get('/', (req, res) => {
-    res.json({"Hello": "World",
-            "Version": 2})
+    res.json({
+        "Hello": "World",
+        "Version": 2
+    })
+})
+
+app.get('/api/health', (req, res) => {
+    res.json({
+        "healthy": true
+    })
 })
 
 app.listen(APP_PORT, () => {
